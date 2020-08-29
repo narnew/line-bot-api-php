a80d74adb1cfb2e7d82fe369a72a00cd8cc50256
narnew <70052481+narnew@users.noreply.github.com>
2020-08-29 17:26:14 +0700

    Update line.php

--- line.php
+++ line.php
@@ -6,7 +6,6 @@ $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' 
 
 $request = file_get_contents('php://input');   // Get request content
 $request_array = json_decode($request, true);   // Decode JSON to Array
-
 if ( sizeof($request_array['events']) > 0 )
 {
 
@@ -20,29 +19,32 @@ if ( sizeof($request_array['events']) > 0 )
    
    if( $event['message']['type'] == 'text' )
    {
-  $text = $event['message']['text'];
-  if($text == "เธอชื่อไรหรอจ๊ะ" ||$text == "ชื่อ"||$text == "ชื่ออะไร"||$text == "ชื่ออะไรครับ"||$text == "ชื่ออะไรคะ"||$text == "name"||$text == "What your name?"){
-   $reply_message = 'เราชื่อว่า แรปบิต เขียนเป็นภาษาอังกฤษได้ว่า Rapbit :);
-  
- }
-    
-         if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
-                 $url = 'https://covid19.th-stat.com/api/open/today';
-                 $ch = curl_init($url);
-                 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-                 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
-                 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
-                 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-                 $result = curl_exec($ch);
-                 curl_close($ch);   
-     
-                 $obj = json_decode($result);
-     
-                 //$reply_message = $result;
-                 $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'}.' คน'.'รักษาหายแล้ว '. $obj->{'Recovered'}.' คน';
-                }
-  //$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
+		$text = $event['message']['text'];
+		
+	   	if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
+			$reply_message = 'ชื่อของฉันคือ BOTCAT';
+		}
+	   
+	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
+		   $url = 'https://covid19.th-stat.com/api/open/today';
+		   $ch = curl_init($url);
+		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		   curl_setopt($ch, CURLOPT_HTTPHEADER, $post_header);
+		   curl_setopt($ch, CURLOPT_POSTFIELDS, $post_body);
+		   curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+		   $result = curl_exec($ch);
+		   curl_close($ch);   
+		   
+		   $obj = json_decode($result);
+		   
+		   //$reply_message = $result;
+		   $reply_message = 'ติดเชื้อสะสม '. $obj->{'Confirmed'} .' คน \r\n รักษาหายแล้ว '.$obj->{'Recovered'} . ' คน';
+	
+	        }
+	   }
+	   
+		//$reply_message = '('.$text.') ได้รับข้อความเรียบร้อย!!';   
    }
    else
     $reply_message = 'ระบบได้รับ '.ucfirst($event['message']['type']).' ของคุณแล้ว';
