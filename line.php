089a097b21ea0ac5e58860c82ccf3156f6997f5a
narnew <70052481+narnew@users.noreply.github.com>
2020-09-01 12:55:54 +0700

    Update line.php

--- line.php
+++ line.php
@@ -22,10 +22,10 @@ if ( sizeof($request_array['events']) > 0 )
 		$text = $event['message']['text'];
 		
 	   	if($text == "ชื่อ" || $text == "ชื่ออะไร" || $text == "ชื่ออะไรครับ"|| $text == "ชื่ออะไรคะ"){
-			$reply_message = 'ชื่อของฉันคือ BOTCAT';
+			$reply_message = 'ชื่อของฉันคือ Rabbit';
 		}
 	   
-	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19"){
+	   	if($text == "สถานการณ์โควิดวันนี้" || $text == "covid19" || $text == "covid-19" || $text == "Covid-19" || $text == "โควิด-19"){
 		   $url = 'https://covid19.th-stat.com/api/open/today';
 		   $ch = curl_init($url);
 		   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
